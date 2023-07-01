0f65b58f1004894e81c3ab0d16ccc4b26bec0845
huyhung27599 <huyhung27599@gmail.com>
2023-07-01 15:45:55 +0700

    init code

---
+++ src/users/user.entity.ts
@@ -0,0 +1,35 @@
+import {
+  AfterInsert,
+  AfterRemove,
+  AfterUpdate,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log('Inserting user with id', this.id);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log('Removing user with id', this.id);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log('Updating user with id', this.id);
+  }
+}
